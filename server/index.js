6d8882cfcbe42c600cc2e136888c677acc8c7827
brian <bdkuhn602@gmail.com>
2022-07-11 03:54:25 -0700

    config google oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-});
+passport.use(
+  new GoogleStategy(
+    {
+      clientID     : keys.googleClientID,
+      clientSecret : keys.googleClientSecret,
+      callbackURL  : '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
