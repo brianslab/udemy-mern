32051fddf05236c01777711feaec9580e967abb8
brian <bdkuhn602@gmail.com>
2022-07-12 01:30:21 -0700

    serialize and deserialize functions

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,6 +6,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStategy(
     {
@@ -16,8 +26,11 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleID: profile.id }).then((existingUser) => {
         if (existingUser) {
+          done(null, existingUser);
         } else {
-          new User({ googleID: profile.id }).save();
+          new User({ googleID: profile.id }).save().then((user) => {
+            done(null, user);
+          });
         }
       });
     }
