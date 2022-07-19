e039977603fc6b12513f0737e7299f21bdaaca5e
brian <bdkuhn602@gmail.com>
2022-07-19 00:31:37 -0700

    setup dev and prod environments

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID     :
+    '911568703721-krapm0lmu74fb7tf4ho82g81qqv9db3m.apps.googleusercontent.com',
+  googleClientSecret : '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',
+  mongoURI           :
+    'mongodb+srv://prod-user:bFgjYnjy7H72g94W@cluster0.6hlzn8n.mongodb.net/?retryWrites=true&w=majority',
+  cookieKey          : 'fTvgCA72ZzMh8Cp5'
+};
