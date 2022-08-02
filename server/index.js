16c63beb994f76ff0fc85f61aad6f090dca53ba8
Brian Kuhn <bdkuhn602@gmail.com>
2022-08-02 07:50:34 -0700

    capture charge from stripe

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/User'); // need to require this before passport
@@ -10,6 +11,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge : 30 * 24 * 60 * 60 * 1000, // 30 days in ms
