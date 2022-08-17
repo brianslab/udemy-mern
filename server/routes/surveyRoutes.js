64934dfa6903b6a43355d548ac4f4e38ad69b2b1
Brian Kuhn <bdkuhn602@gmail.com>
2022-08-17 00:37:19 -0700

    begin survey creation route handler

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -8,5 +8,11 @@ const Survey = mongoose.model('surveys');
 module.exports = (app) => {
   app.post('api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body
+    });
   });
 };
